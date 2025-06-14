525f4453b54444d92b62153e993930722ce7b35d
Yeoeun Lee <haepi562@gmail.com>
2025-06-14 22:22:01 +0900

    상수화 작업_색상,폰트,아이콘경로

---
+++ src/constants/iconpath.ts
@@ -0,0 +1,31 @@
+export const ICONPATH = {
+
+  ADMIN: {
+    CHAT: '/icons/admin_chat.svg',
+    DELETE: '/icons/admin_delete.svg',
+    MENU_GREEN: '/icons/admin_menu_green.svg',
+    PEOPLE: '/icons/admin_people.svg',
+    USERS: '/icons/admin_users_profile.svg',
+    VOICE: '/icons/admin_Voice.svg',
+  },
+
+  HOME: {
+    ALARM_DISABLED: '/icons/home_alarm_disabled.svg',
+    ALERT_MESSAGE: '/icons/home_alert_message.svg',
+    HOUSE_GRAY: '/icons/home_house_gray.svg',
+    MESSAGE_FILLED: '/icons/home_message_filled.svg',
+    SEARCH_BLUE: '/icons/home_search_blue.svg',
+    CHEVRON: '/icons/home_chevron_right.svg',
+  },
+
+  MYPAGE: {
+    HOME: '/icons/mypage_home.svg',
+    SMILE: '/icons/mypage_smile.svg',
+    CRY: '/icons/mypage_cry.svg',
+  },
+
+  HEART: '/icons/heart.svg',
+  CHEVRON_LEFT: '/icons/chevron_left.svg',
+  X: '/icons/x.svg',
+
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+import { COLORS } from './src/constants/colors';
+import { TYPOGRAPHY } from './src/constants/typography';
+
 export default {
-  content: [],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Tailwind가 클래스 인식할 파일 경로 추가
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 기본 색상
+        primary: COLORS.PRIMARY,
+
+        // 세컨더리 색상들
+        secondary: {
+          hw_red1: COLORS.SECONDARY.HW_Red1,
+          hw_red2: COLORS.SECONDARY.HW_Re2,
+          hw_green1: COLORS.SECONDARY.HW_Green1,
+          hw_green2: COLORS.SECONDARY.HW_Green2,
+          hw_green3: COLORS.SECONDARY.HW_Green3,
+          hw_blue1: COLORS.SECONDARY.HW_Blue1,
+          hw_blue2: COLORS.SECONDARY.HW_Blue2,
+        },
+
+        // 그레이스케일 색상들
+        grayscale: {
+          f: COLORS.GRAYSCALE.F,
+          g2: COLORS.GRAYSCALE.G2,
+          g3: COLORS.GRAYSCALE.G3,
+        },
+      },
+
+      fontFamily: {
+        // Pretendard 등록
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
