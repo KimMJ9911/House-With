a86b3ffc86fac4c53193f546da90f819e6379cc2
Yeoeun Lee <haepi562@gmail.com>
2025-06-15 14:30:26 +0900

    fork 작업물을 원본 레포의 새로운 브랜치에 반영

---
+++ src/constants/colors.ts
@@ -0,0 +1,28 @@
+export const COLORS = {
+
+  PRIMARY: '#FE8B26',
+
+  SECONDARY: {
+    HW_Red1: '#FBF1F2',
+    HW_Re2: '#FC5223',
+    HW_Green1: '#DFF5EB',
+    HW_Green2: '#5DC899',
+    HW_Green3: '#00DB5D',
+    HW_Blue1: '#EAF3FE',
+    HW_Blue2: '#0366FF',
+  },
+
+  GRAYSCALE: {
+    F: '#FFFFFF',
+    G2: '#E6E6E6',
+    G3: '#CCCCCC',
+    G4: '#9F9F9F',
+    G5: '#909090',
+    G6: '#828282',
+    G7: '#737373',
+    G8: '#555555',
+    G9: '#383838',
+    B: '#1A1A1A',
+  },
+
+}; 
